37a798e083742182b2db513856520b15a69816e5
syahirazman <syahirahnorazman@gmail.com>
2022-12-28 20:32:22 +0800

    updated interface and animation

--- app/src/main/java/com/example/maribertutur/MainActivity.java
+++ app/src/main/java/com/example/maribertutur/MainActivity.java
@@ -5,6 +5,8 @@ import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.VideoView;
+import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity {
     public Button FirstBtn;
--- app/src/main/java/com/example/maribertutur/SebutanPerkataan_activity.java
+++ app/src/main/java/com/example/maribertutur/SebutanPerkataan_activity.java
@@ -1,11 +1,17 @@
 package com.example.maribertutur;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SebutanPerkataan_activity extends AppCompatActivity implements View.OnClickListener {
 

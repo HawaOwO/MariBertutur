b39e811216a9506601ac6a546a55829be0ef12aa
syahirazman <syahirahnorazman@gmail.com>
2022-12-29 01:41:30 +0800

    Make minor changes

--- app/src/main/java/com/example/maribertutur/Permainan_Bacaan.java
+++ app/src/main/java/com/example/maribertutur/Permainan_Bacaan.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Permainan_Bacaan extends AppCompatActivity implements View.OnClickListener {
